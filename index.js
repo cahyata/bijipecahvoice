1e428ff14b998a3db0cce1fff8be9ac039226205
cahyata <guardbros06@gmail.com>
2021-07-27 15:32:46 +0700

    event handler

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const discord = require("discord.js")
+const Discord = require("discord.js")
 const fs = require("fs");
 const { MessageEmbed } = require("discord.js");
 const express = require("express");
@@ -22,18 +23,12 @@ const client = new discord.Client({
   
   app.listen(process.env.PORT || 5000);
 
-  client.commands = new discord.Collection();
-client.aliases = new discord.Collection();
+  client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
 
-
-  fs.readdir(__dirname + "/events/", (err, files) => {
-    if (err) return console.error(err);
-    files.forEach((file) => {
-      const event = require(__dirname + `/events/${file}`);
-      let eventName = file.split(".")[0];
-      client.on(eventName, event.bind(null, client));
-      console.log("Loading Event: "+eventName)
-    });
+  fs.readdir('./events/', (err, files) => {
+    const eventHandler = require('./handler/eventHandler.js');
+    eventHandler(err, files, client);
   });
 
   fs.readdir('./commands/', (err, files) => {
