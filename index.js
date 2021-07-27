a7c372181b02b601c85663db309fb0c14a3664f4
cahyata <guardbros06@gmail.com>
2021-07-27 15:08:17 +0700

    fix Cannot access 'client' before initialization

--- index.js
+++ index.js
@@ -5,15 +5,6 @@ const { MessageEmbed } = require("discord.js");
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-    res.sendStatus(200);
-  });
-  
-  app.listen(process.env.PORT || 5000);
-
-  client.commands = new discord.Collection();
-client.aliases = new discord.Collection();
-
 const client = new discord.Client({
     ws: {
       intents: [
@@ -24,6 +15,15 @@ const client = new discord.Client({
       ],
     },
   });
+
+  app.get("/", (req, res) => {
+    res.sendStatus(200);
+  });
+  
+  app.listen(process.env.PORT || 5000);
+
+  client.commands = new discord.Collection();
+client.aliases = new discord.Collection();
 
 
   fs.readdir(__dirname + "/events/", (err, files) => {
