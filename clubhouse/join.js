102487bbee6866caddcd5cd35782714b18d83b35
cahyata <guardbros06@gmail.com>
2021-07-27 16:11:11 +0700

    clubhouse

---
+++ clubhouse/join.js
@@ -0,0 +1,14 @@
+const { guildOnly } = require("../commands/ping")
+
+module.exports = client => {
+    const masterch = client.channels.cache.get('869162393756921876')
+
+    client.on('voiceStateUpdate', (oldState, newState) => {
+        if(oldState.channelID === null && newState.channelID !== null && newState.channelID !== "869162393756921876"){
+            console.log('OKe')
+            guild.channels.create('tes', {
+                type: 'voice'
+            })
+        }
+    })
+}
