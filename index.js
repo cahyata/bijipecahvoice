062dfb2d94053c696f181165c114853790d25d25
cahyata <guardbros06@gmail.com>
2021-07-27 11:48:22 +0700

    port

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 require('dotenv').config()
 const discord = require("discord.js")
+const express = require("express");
+const app = express();
 
+app.get("/", (req, res) => {
+    res.sendStatus(200);
+  });
+  
+  app.listen(process.env.PORT || 5000);
 
 const client = new discord.Client({
     ws: {
